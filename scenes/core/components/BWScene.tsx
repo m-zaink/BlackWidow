56a7be81b2dd210cf462bf05905afed6a724415a
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-06-12 16:57:08 +0530

    Update BWScene

--- scenes/core/components/BWScene.tsx
+++ scenes/core/components/BWScene.tsx
@@ -1,7 +1,12 @@
-import { StyleSheet, View, ViewStyle } from "react-native";
+import { StyleProp, StyleSheet, View, ViewStyle } from "react-native";
 
-const BWScene = (props: { style: ViewStyle; children: React.ReactNode }) => {
-    return <View style={[styles.sceneStyle, props.style]}>{props.children}</View>;
+const BWScene = (props: {
+    style: StyleProp<ViewStyle>;
+    children: React.ReactNode;
+}) => {
+    return (
+        <View style={[styles.sceneStyle, props.style]}>{props.children}</View>
+    );
 };
 
 const styles = StyleSheet.create({
