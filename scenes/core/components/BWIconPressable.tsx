cf2cd6e7b7aa3e0f566659f1bc02e355d7987e22
Mohammed Sadiq <mzainkofficial@gmail.com>
2022-06-12 16:58:58 +0530

    Add BWIconPressable

---
+++ scenes/core/components/BWIconPressable.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+import { StyleProp, ViewStyle } from "react-native";
+import { StateWiseStyleCallback, VoidCallback } from "../types";
+import BWPressable from "./BWPressable";
+
+const BWIconPressable = (props: {
+    icon: ReactNode;
+    style?: StyleProp<ViewStyle> | StateWiseStyleCallback<{ pressed: Boolean }>;
+    onPress?: VoidCallback;
+}) => {
+    return (
+        <BWPressable
+            onPress={props.onPress}
+            style={
+                props.style ||
+                (({ pressed }) =>
+                    pressed ? { opacity: 0.3 } : { opacity: 1.0 })
+            }
+        >
+            {props.icon}
+        </BWPressable>
+    );
+};
+
+export default BWIconPressable;
